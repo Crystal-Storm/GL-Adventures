5bf2bd10a16d66dbb80467c100434d27758f3991
Storm <storm.minecrafter.sss@gmail.com>
2025-03-14 02:59:30 -0400

    switched from glfw to SDL2, nightmare nightmare nightmare

--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,99 @@
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
 #include <iostream>
+#include <SDL2/SDL.h>
+#include <glad/glad.h>
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
-    glViewport(0, 0, width, height);
+// Globals
+int gScreenWidth = 640;
+int gScreenHeight = 480;
+SDL_Window* gGraphicsApplicationWindow = nullptr;
+SDL_GLContext gOpenGLContext = nullptr;
+
+bool gQuit = false;
+
+// void GetOpenGLVersionInfo(){
+//     std::cout << "Vendor: " << glGetString(GL_VENDOR) << std::endl;
+//     std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
+//     std::cout << "Version: " << glGetString(GL_VERSION) << std::endl;
+//     std::cout << "Shading Language: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << std::endl;
+// }
+
+void InitializeProgram(){
+    if(SDL_Init(SDL_INIT_VIDEO)<0){
+        std::cout << "SDL2 could not initialize video subsystem" << std::endl;
+        exit(1);
+    }
+
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+
+    gGraphicsApplicationWindow = SDL_CreateWindow("SDL game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, gScreenWidth, gScreenHeight, SDL_WINDOW_OPENGL);
+
+    if(gGraphicsApplicationWindow==nullptr){
+        std::cout << "SDL_Window was not able to be created" << std::endl;
+        exit(1);
+    }
+
+    gOpenGLContext = SDL_GL_CreateContext(gGraphicsApplicationWindow);
+
+    if(gOpenGLContext==nullptr){
+        std::cout << "OpenGL context not available" << std::endl;
+        exit(1);
+    }
 }
 
-int main() {
-    // Initialize GLFW
-    if (!glfwInit()) {
-        std::cerr << "Failed to initialize GLFW" << std::endl;
-        return -1;
+void Input(){
+    SDL_Event e;
+
+    while(SDL_PollEvent(&e) != 0){
+        if (e.type==SDL_QUIT){
+            // std::cout << "Goodbye" << std::endl;
+            gQuit = true;
+        }
     }
+}
 
-    // Set OpenGL version to 3.3
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+void PreDraw(){
 
-    // Create a windowed mode window and its OpenGL context
-    GLFWwindow* window = glfwCreateWindow(800, 600, "First game", nullptr, nullptr);
-    if (!window) {
-        std::cerr << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
+}
+
+void Draw(){
+
+}
+
+void MainLoop(){
+    while(!gQuit){
+        Input();
+
+        PreDraw();
+
+        Draw();
+
+        // Update screen
+        SDL_GL_SwapWindow(gGraphicsApplicationWindow);
     }
+}
 
-    // Make the window's context current
-    glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+void CleanUp(){
 
-    // Load OpenGL functions using Glad
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-        std::cerr << "Failed to initialize Glad" << std::endl;
-        return -1;
-    }
+    // Destroy the window
+    SDL_DestroyWindow(gGraphicsApplicationWindow);
 
-    // Main loop
-    while (!glfwWindowShouldClose(window)) {
-        // Render here
-        glClear(GL_COLOR_BUFFER_BIT);
+    // Quit SDL subsystems
+    SDL_Quit();
+}
 
-        // Swap front and back buffers
-        glfwSwapBuffers(window);
+int main(int argc, char* argv[]){
 
-        // Poll for and process events
-        glfwPollEvents();
-    }
+    std::cout << "Test" << std::endl;
+    InitializeProgram();
 
-    // Clean up
-    glfwTerminate();
+    MainLoop();
+
+    CleanUp();
+
     return 0;
 }
