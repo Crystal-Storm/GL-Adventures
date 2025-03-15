d4775be120002b399264953c03f50e685701fe8f
Storm <storm.minecrafter.sss@gmail.com>
2025-03-14 20:20:29 -0400

    changed shaders

--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -1,5 +1,7 @@
 #version 410 core
 in vec4 position;
+out vec3 vertexColor;
 void main(){
+    vertexColor = vec3(.5,.5,.5);
     gl_Position = vec4(position.x, position.y, position.z, position.w);
 }
